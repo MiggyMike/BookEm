f24ee191593f75de5125eaef845734bd71e158c9
MICHAEL <MJENNINGS6@gmail.com>
2021-01-07 08:32:03 -0500

    set layout

---
+++ client/src/components/Layout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
+
+const Layout = (props) => {
+  return (
+    <div>
+      <Header
+        {...props}
+        authenticated={props.authenticated}
+        currentUser={props.currentUser}
+        toggleAuthenticated={props.toggleAuthenticated}
+      />
+      {props.children}
+
+      <Footer />
+    </div>
+  );
+};
+
+export default withRouter(Layout);
