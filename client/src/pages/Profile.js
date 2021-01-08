483b332eeda3c2a1d86f8e15f8a475b80e06a2b2
MICHAEL <MJENNINGS6@gmail.com>
2021-01-08 15:53:04 -0500

    profiles have their services/products

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,23 +1,71 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
-import { Row, Col } from 'react-bootstrap';
+import { Card, Row, Col, Container } from 'react-bootstrap';
+import { __GetProfile } from '../services/UserServices';
 
 const Profile = (props) => {
   console.log('profile:', props);
+
+  const [services, setServices] = useState([]);
+
+  const getUserServs = async () => {
+    try {
+      const res = await __GetProfile(props.currentUser._id);
+      setServices(res.services);
+      console.log('PRF:', res.services);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    getUserServs();
+  }, []);
+
   return (
     <div>
-      <h1>Profile</h1>
-      <h4>Meet {props.currentUser.name}</h4>
-      {/* need to structure this and pull by user id */}
-      {/* <p>{props.services[0].service}</p> */}
-      {/* <Row>
-        {props.services.map((service) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
-            <h3>{props.services.service}</h3>
-          </Col>
-        ))}
+      <Container>
+        <h1>Profile</h1>
+        <h4>Welcome to your profile</h4>
+        {/* <p>{props.services[2].price}</p> */}
+        {/* need to structure this and pull by user id */}
+        {/* <p>{props.services[0].service}</p> */}
+        <Row>
+          {services.length ? (
+            <div>
+              {services.map((service) => (
+                <div key={service._id}>
+                  <Card style={{ width: '15rem' }}>
+                    <div>
+                      <div>
+                        <h3>{service.title}</h3>
+                        <p>{service.description}</p>
+                      </div>
+                    </div>
+                    <img src={service.image_url} alt='sf' />
+                  </Card>
+                  <div>
+                    <button>Edit</button>
+                    <button onClick={() => this.deleteservice(service._id)}>
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className='span message'>No Services Available</div>
+          )}
+        </Row>
+        {/* <Row>
+        {service.length &&
+          props.services.map((service) => (
+            <Col sm={12} md={6} lg={4} xl={3}>
+              <h3>{props.services.service}</h3>
+            </Col>
+          ))}
       </Row> */}
-      {console.log('profile:', props)};
+      </Container>
     </div>
   );
 };
