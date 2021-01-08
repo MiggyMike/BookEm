f8a33891c24b5f301096065294443ad68e7aa59c
MICHAEL <MJENNINGS6@gmail.com>
2021-01-08 00:08:15 -0500

    added a few get service and services fts for testing

--- client/src/services/ServiceServices.js
+++ client/src/services/ServiceServices.js
@@ -9,3 +9,32 @@ export const __CreateService = async (formData, userId) => {
     throw error;
   }
 };
+
+export const __GetServices = async (page, limit) => {
+  try {
+    const res = await ApiClient.get(
+      `/services?page=${page || 1}&limit=${limit || 10}`
+    );
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __GetServices_2 = async (userId) => {
+  try {
+    const res = await ApiClient.get(`/services/user/${userId}`);
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const __GetService = async (serviceId) => {
+  try {
+    const res = await ApiClient.get(`/services/${serviceId}`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
