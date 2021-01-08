84ea3aae7f231d81ba7d78ab204c87130b7e3abc
MICHAEL <MJENNINGS6@gmail.com>
2021-01-08 00:03:34 -0500

    displaying all services

--- client/src/pages/Services.js
+++ client/src/pages/Services.js
@@ -1,12 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
+import { __GetServices } from '../services/ServiceServices';
+import Service from '../components/Service';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Services = (props) => {
+  // console.log('SRV:', props);
+  const [services, setServices] = useState([]);
+
+  const getServs = async () => {
+    try {
+      const res = await __GetServices();
+      setServices(res);
+      console.log('SRV:', res);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    getServs();
+  }, []);
+
+  console.log('SRV2:', props);
   return (
     <div>
-      <h1>Services</h1>
+      <Container>
+        <h1>Services</h1>
+        <Row>
+          {services.length &&
+            services.map((service, index) => (
+              <Col sm={12} md={4} lg={4} xl={3}>
+                <Service
+                  // props={props}
+                  service={service.service}
+                  description={service.description}
+                  image_url={service.image_url}
+                  duration={service.duration}
+                  price={service.price}
+                  rating={service.rating}
+                  numReviews={service.numReviews}
+                />
+              </Col>
+            ))}
+        </Row>
+      </Container>
     </div>
   );
 };
 
 export default withRouter(Services);
+
+{
+  /* <Card>
+<a href={`/service/${service._id}`}>
+  <Card.Img src={service.image} variant='top' />
+</a>
+</Card>
+image_url={service.image_url} */
+}
