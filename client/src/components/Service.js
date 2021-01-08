3ef7b65d13ec33fda3add93f3f07b635173f24e5
MICHAEL <MJENNINGS6@gmail.com>
2021-01-08 12:31:27 -0500

    services set without user needed being logged in

--- client/src/components/Service.js
+++ client/src/components/Service.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardGroup } from 'react-bootstrap';
 
 const Service = ({
+  props,
   service,
   description,
   image_url,
@@ -9,23 +10,29 @@ const Service = ({
   price,
   rating,
   numReviews,
+  user_id,
 }) => {
-  console.log('SC:');
+  console.log('SC:', props);
 
   return (
-    <CardGroup>
-      <Card lg={3}>
-        <Card.Img variant='top' src='{image_url}' />
-        <Card.Body>
-          <Card.Title> {service}</Card.Title>
-          <Card.Text>{description}</Card.Text>
-          <Card.Text>{duration}</Card.Text>
-          <Card.Text>{price}</Card.Text>
-          <Card.Text>{rating}</Card.Text>
-          <Card.Text>{numReviews}</Card.Text>
-        </Card.Body>
-      </Card>
-    </CardGroup>
+    <div>
+      <CardGroup>
+        <Card>
+          <Card.Img variant='top' src='holder.js/100px160' />
+          <Card.Body>
+            <Card.Title>{service}</Card.Title>
+            <Card.Text>{description}</Card.Text>
+            <Card.Text>{duration}</Card.Text>
+            <Card.Text>{price}</Card.Text>
+            <Card.Text>{rating}</Card.Text>
+            <Card.Text>{numReviews}</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className='text-muted'>User name</small>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
+    </div>
   );
 };
 
