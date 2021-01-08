1b49160f5a74a2b3d0bab703adc84dc075865c2f
MICHAEL <MJENNINGS6@gmail.com>
2021-01-08 12:43:30 -0500

    updated card style

--- client/src/pages/Services.js
+++ client/src/pages/Services.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import { __GetServices } from '../services/ServiceServices';
 import Service from '../components/Service';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Card, CardGroup, Container, Row, Col } from 'react-bootstrap';
 
 const Services = (props) => {
   // console.log('SRV:', props);
@@ -30,18 +30,22 @@ const Services = (props) => {
         <Row>
           {services.length &&
             services.map((service, index) => (
-              <Col sm={12} md={4} lg={4} xl={3}>
-                <Service
-                  // props={props}
-                  service={service.service}
-                  description={service.description}
-                  image_url={service.image_url}
-                  duration={service.duration}
-                  price={service.price}
-                  rating={service.rating}
-                  numReviews={service.numReviews}
-                />
-              </Col>
+              <CardGroup>
+                <Card style={{ width: '15rem' }}>
+                  <Card.Img variant='top' src={service.image_url} />
+                  <Card.Body>
+                    <Card.Title>{service.service}</Card.Title>
+                    <Card.Text>{service.description}</Card.Text>
+                    <Card.Text>{service.duration}</Card.Text>
+                    <Card.Text>{service.price}</Card.Text>
+                    <Card.Text>{service.rating}</Card.Text>
+                    <Card.Text>{service.numReviews}</Card.Text>
+                  </Card.Body>
+                  <Card.Footer>
+                    <small className='text-muted'>User name</small>
+                  </Card.Footer>
+                </Card>
+              </CardGroup>
             ))}
         </Row>
       </Container>
@@ -50,12 +54,3 @@ const Services = (props) => {
 };
 
 export default withRouter(Services);
-
-{
-  /* <Card>
-<a href={`/service/${service._id}`}>
-  <Card.Img src={service.image} variant='top' />
-</a>
-</Card>
-image_url={service.image_url} */
-}
