013c686f1561b05fca4369051275c9993cff907d
MICHAEL <MJENNINGS6@gmail.com>
2021-01-09 00:18:36 -0500

    adjusted color

--- client/src/components/Rating.js
+++ client/src/components/Rating.js
@@ -15,18 +15,7 @@ const Rating = ({ value, text, color }) => {
           }
         ></i>
       </span>
-      <span>
-        <i
-          className={
-            value >= 1
-              ? 'fas fa-star'
-              : value >= 0.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
-          }
-        ></i>
-      </span>
-      <span>
+      <>
         <i
           style={{ color }}
           className={
@@ -37,7 +26,7 @@ const Rating = ({ value, text, color }) => {
               : 'far fa-star'
           }
         ></i>
-      </span>
+      </>
       <span>
         <i
           style={{ color }}
@@ -80,7 +69,7 @@ const Rating = ({ value, text, color }) => {
 };
 
 Rating.defaultProps = {
-  color: '#f8e825',
+  color: '#fdb827',
 };
 
 export default Rating;
