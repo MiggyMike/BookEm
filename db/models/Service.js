1975381ee92e27a590846d8bad93565d3066aa5f
MICHAEL <MJENNINGS6@gmail.com>
2021-01-09 00:20:33 -0500

    added user name to service schema

--- db/models/Service.js
+++ db/models/Service.js
@@ -47,6 +47,11 @@ module.exports = new Schema(
         ref: 'reviews',
       },
     ],
+    user_name: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'users',
+    },
   },
   { timestamps: true }
 );
