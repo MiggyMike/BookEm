5deab462cb8956472172c4b53b983762fe253b01
MICHAEL <MJENNINGS6@gmail.com>
2021-01-09 23:23:13 -0500

    styled

--- client/src/pages/CreateServices.js
+++ client/src/pages/CreateServices.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { __CreateService } from '../services/ServiceServices';
-import { Form, Button, Container } from 'react-bootstrap';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 
 function CreateServices(props) {
   // console.log('props:', props.currentUser._id);
@@ -65,85 +65,97 @@ function CreateServices(props) {
     <div>
       <Container>
         <h1>Create Service</h1>
+        <Col lg={8} className='mx-auto'>
+          <Form
+            className='form-content-center'
+            onSubmit={(e) => handleSubmit(e)}
+          >
+            <Form.Group controlId='formGroupName'>
+              <Form.Label>Service</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Service Name'
+                name='service'
+                className='form-input'
+                onChange={handleService}
+              />
+            </Form.Group>
+            <Form.Group controlId='exampleForm.ControlTextarea1'>
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                as='textarea'
+                rows={3}
+                type='text'
+                placeholder='Describe service'
+                name='description'
+                className='form-input'
+                onChange={handleDescription}
+              />
+            </Form.Group>
+            <Form.Group controlId='exampleForm.ControlTextarea1'>
+              <Form.Label>Image</Form.Label>
+              <Form.Control
+                as='textarea'
+                rows={3}
+                type='text'
+                placeholder='paste img url/file here'
+                name='image_url'
+                className='form-input'
+                onChange={handleImage}
+              />
+            </Form.Group>
+            <Row className='mx-auto'>
+              <Form.Group
+                controlId='formGroupDuration'
+                className='mr-auto w-25'
+              >
+                <Form.Label>Duration</Form.Label>
+                <Form.Control
+                  type='text'
+                  placeholder='1hr 30min'
+                  name='duration'
+                  className='form-input'
+                  onChange={handleDuration}
+                />
+              </Form.Group>
+              <Form.Group controlId='formGroupPrice' className='mr-auto w-25'>
+                <Form.Label>Price</Form.Label>
+                <Form.Control
+                  type='number'
+                  placeholder='00.00'
+                  name='price'
+                  className='form-input'
+                  onChange={handlePrice}
+                />
+              </Form.Group>
+              <Form.Group controlId='select' className='mr-auto '>
+                <Form.Label>Rating</Form.Label>
+                <Form.Control
+                  type='number'
+                  min='0'
+                  max='5'
+                  placeholder='0'
+                  name='rating'
+                  className='form-input'
+                  onChange={handleRating}
+                />
+              </Form.Group>
+              <Form.Group controlId='formGroupPrice' className='mr-auto w-25'>
+                <Form.Label># of Reviews</Form.Label>
+                <Form.Control
+                  type='number'
+                  min='0'
+                  placeholder='0'
+                  name='reviews'
+                  className='form-input'
+                  onChange={handleNumReviews}
+                />
+              </Form.Group>
+            </Row>
 
-        <Form className='form-content-center' onSubmit={(e) => handleSubmit(e)}>
-          <Form.Group controlId='formGroupName'>
-            <Form.Label>Service</Form.Label>
-            <Form.Control
-              type='text'
-              placeholder='Service Name'
-              name='service'
-              className='form-input'
-              onChange={handleService}
-            />
-          </Form.Group>
-          <Form.Group controlId='exampleForm.ControlTextarea1'>
-            <Form.Label>Description</Form.Label>
-            <Form.Control
-              as='textarea'
-              rows={3}
-              type='text'
-              placeholder='Describe service'
-              name='description'
-              className='form-input'
-              onChange={handleDescription}
-            />
-          </Form.Group>
-          <Form.Group controlId='exampleForm.ControlTextarea1'>
-            <Form.Label>Image</Form.Label>
-            <Form.Control
-              as='textarea'
-              rows={3}
-              type='text'
-              placeholder='paste img url/file here'
-              name='image_url'
-              className='form-input'
-              onChange={handleImage}
-            />
-          </Form.Group>
-          <Form.Group controlId='formGroupDuration'>
-            <Form.Label>Duration</Form.Label>
-            <Form.Control
-              type='text'
-              placeholder='1hr 30min'
-              name='duration'
-              className='form-input'
-              onChange={handleDuration}
-            />
-          </Form.Group>
-          <Form.Group controlId='formGroupPrice'>
-            <Form.Label>Price</Form.Label>
-            <Form.Control
-              type='text'
-              placeholder='00.00'
-              name='price'
-              className='form-input'
-              onChange={handlePrice}
-            />
-          </Form.Group>
-          <Form.Group controlId='formGroupPrice'>
-            <Form.Label>Rating</Form.Label>
-            <Form.Control
-              type='text'
-              placeholder='0'
-              name='rating'
-              className='form-input'
-              onChange={handleRating}
-            />
-          </Form.Group>
-          <Form.Group controlId='formGroupPrice'>
-            <Form.Label># of Reviews</Form.Label>
-            <Form.Control
-              type='text'
-              placeholder='0'
-              name='reviews'
-              className='form-input'
-              onChange={handleNumReviews}
-            />
-          </Form.Group>
-
-          <Button type='submit'>Submit</Button>
-        </Form>
+            <Button type='submit'>Submit</Button>
+          </Form>
+        </Col>
       </Container>
     </div>
   );
