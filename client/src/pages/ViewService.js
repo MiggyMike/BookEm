026eae9f60d9dead612b2a7dc5cd2a62e8bd9436
MICHAEL <MJENNINGS6@gmail.com>
2021-01-09 19:06:07 -0500

    adjusted layout for viewing service

--- client/src/pages/ViewService.js
+++ client/src/pages/ViewService.js
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { __GetServiceById } from '../services/ServiceServices';
 import { Link, withRouter } from 'react-router-dom';
-import { Container, Row, Col, Image, Card, Button } from 'react-bootstrap';
+import {
+  Container,
+  Row,
+  Col,
+  Image,
+  Card,
+  Button,
+  ListGroup,
+} from 'react-bootstrap';
 import Rating from '../components/Rating';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const ViewService = (props) => {
   const [service, setService] = useState({});
@@ -25,7 +34,7 @@ const ViewService = (props) => {
   return (
     <div>
       <Container>
-        <h1>{service.service}</h1>
+        {/* <h1>{service.service}</h1>
         <Rating value={service.rating} text={`${service.numReviews} reviews`} />
         <Image
           src={service.image_url}
@@ -36,7 +45,57 @@ const ViewService = (props) => {
         <p>{service.description}</p>
 
         <p>{service.duration}</p>
-        <p>${service.price}</p>
+        <p>${service.price}</p> */}
+
+        <Link to='/services'>
+          <Button className=' btn-light my-3'>Go Back</Button>
+        </Link>
+        <Row>
+          <Col md={6}>
+            <Image
+              src={service.image_url}
+              alt={service.service}
+              style={{ objectFit: 'fit' }}
+              fluid
+            ></Image>
+          </Col>
+          <Col md={3}>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <h2>{service.service}</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Rating
+                  value={service.rating}
+                  text={`${service.numReviews} reviews`}
+                />
+              </ListGroup.Item>
+              <ListGroup.Item>Price: ${service.price}</ListGroup.Item>
+              <ListGroup.Item>
+                Description: {service.description}
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col>
+            <Card>
+              <ListGroup variant='flush'>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <stong> ${service.price}</stong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Button className='btn-block' type='button'>
+                    Book
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
