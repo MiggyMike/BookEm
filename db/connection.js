47093ddc91a4d6c14b529e367525eb7616f0d13b
MICHAEL <MJENNINGS6@gmail.com>
2021-01-11 17:39:04 -0500

    deployed

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.connect('mongodb://localhost:27017/book-me', {
-  useNewUrlParser: true,
-  useFindAndModify: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+const MONGO_URI = process.env.MONGO_URI;
+
+const connection = mongoose.connect(
+  process.env.NODE_ENV === 'production'
+    ? MONGO_URI
+    : 'mongodb://localhost:27017/book-me',
+  {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }
+);
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const AppRouter = require('./routes/AppRouter');
 const express = require('express');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
-
+///mongo heroku deployment
+const path = require('path');
+//
 const logger = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -10,7 +12,6 @@ const connection = require('./db/connection');
 
 const dotenv = require('dotenv');
 const colors = require('colors');
-const path = require('path');
 
 dotenv.config();
 
@@ -20,15 +21,23 @@ const app = express();
 
 // middleware setup
 app.use(logger('dev'));
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false })); // updated for mongo deployment
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+///mongo heroku deployment
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+//
 
 app.disable('X-Powered-By');
 app.get('/', (req, res) => res.send('Server is running'));
 app.use('/api', AppRouter);
+///mongo heroku deployment
 
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+);
+//
 // custom error message handler
 app.use(notFound);
 app.use(errorHandler);
