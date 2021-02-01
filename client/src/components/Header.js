b74e0aa1c8807cb1a3affb5d9ee39d8686401c3c
MICHAEL <MJENNINGS6@gmail.com>
2021-02-01 12:18:23 -0500

    header update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,78 +4,87 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 
 const Header = (props) => {
-  // console.log('header:', props.currentUser);
+    // console.log('header:', props.currentUser);
 
-  return (
-    <header>
-      {props.authenticated && props.currentUser ? (
-        <Navbar bg='light' expand='lg' collapseOnSelect>
-          <Container>
-            <LinkContainer to='/'>
-              <Navbar.Brand>BookIt</Navbar.Brand>
-            </LinkContainer>
-            <Navbar.Toggle aria-controls='basic-navbar-nav' />
-            <Navbar.Collapse id='basic-navbar-nav'>
-              <Nav className='ml-auto'>
-                <LinkContainer
-                  to='/'
-                  onClick={() =>
-                    props.toggleAuthenticated(false, null, () =>
-                      props.history.push('/')
-                    )
-                  }
-                >
-                  <Nav.Link>Log Out</Nav.Link>
-                </LinkContainer>
-                {/* <LinkContainer to='/reviews'>
+    return (
+        <header>
+            {props.authenticated && props.currentUser ? (
+                <Navbar bg='light' expand='lg' collapseOnSelect>
+                    <Container>
+                        <LinkContainer to='/'>
+                            <Navbar.Brand>BookEm</Navbar.Brand>
+                        </LinkContainer>
+                        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                        <Navbar.Collapse id='basic-navbar-nav'>
+                            <Nav className='ml-auto'>
+                                <LinkContainer
+                                    to='/'
+                                    onClick={() =>
+                                        props.toggleAuthenticated(
+                                            false,
+                                            null,
+                                            () => props.history.push('/')
+                                        )
+                                    }>
+                                    <Nav.Link>Log Out</Nav.Link>
+                                </LinkContainer>
+                                {/* <LinkContainer to='/reviews'>
                   <Nav.Link>Reviews</Nav.Link>
                 </LinkContainer> */}
-                <LinkContainer to='/services'>
-                  <Nav.Link>Services</Nav.Link>
-                </LinkContainer>
-                {/* <LinkContainer to='/#'>
+                                <LinkContainer to='/services'>
+                                    <Nav.Link>Services</Nav.Link>
+                                </LinkContainer>
+                                {/* <LinkContainer to='/#'>
                   <Nav.Link>Books</Nav.Link>
                 </LinkContainer> */}
-                <NavDropdown title='Acct' id='basic-nav-dropdown'>
-                  <LinkContainer to='/profile'>
-                    <NavDropdown.Item>Profile</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to='/create'>
-                    <NavDropdown.Item>Post Service</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to='/action'>
-                    <NavDropdown.Item>Settings</NavDropdown.Item>
-                  </LinkContainer>
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      ) : (
-        <Navbar bg='light' expand='lg' collapseOnSelect>
-          <Container>
-            <LinkContainer to='/'>
-              <Navbar.Brand>BookIt</Navbar.Brand>
-            </LinkContainer>
-            <Navbar.Toggle aria-controls='basic-navbar-nav' />
-            <Navbar.Collapse id='basic-navbar-nav'>
-              <Nav className='ml-auto'>
-                <LinkContainer to='/login'>
-                  <Nav.Link>Log In</Nav.Link>
-                </LinkContainer>
-                {/* <LinkContainer to='/reviews'>
+                                <NavDropdown
+                                    title='Acct'
+                                    id='basic-nav-dropdown'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>
+                                            Profile
+                                        </NavDropdown.Item>
+                                    </LinkContainer>
+                                    <LinkContainer to='/create'>
+                                        <NavDropdown.Item>
+                                            Post Service
+                                        </NavDropdown.Item>
+                                    </LinkContainer>
+                                    <LinkContainer to='/action'>
+                                        <NavDropdown.Item>
+                                            Settings
+                                        </NavDropdown.Item>
+                                    </LinkContainer>
+                                </NavDropdown>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            ) : (
+                <Navbar bg='light' expand='lg' collapseOnSelect>
+                    <Container>
+                        <LinkContainer to='/'>
+                            <Navbar.Brand>BookIt</Navbar.Brand>
+                        </LinkContainer>
+                        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                        <Navbar.Collapse id='basic-navbar-nav'>
+                            <Nav className='ml-auto'>
+                                <LinkContainer to='/login'>
+                                    <Nav.Link>Log In</Nav.Link>
+                                </LinkContainer>
+                                {/* <LinkContainer to='/reviews'>
                   <Nav.Link>Reviews</Nav.Link>
                 </LinkContainer> */}
-                <LinkContainer to='/services'>
-                  <Nav.Link>Services</Nav.Link>
-                </LinkContainer>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      )}
-    </header>
-  );
+                                <LinkContainer to='/services'>
+                                    <Nav.Link>Services</Nav.Link>
+                                </LinkContainer>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            )}
+        </header>
+    );
 };
 
 export default withRouter(Header);
